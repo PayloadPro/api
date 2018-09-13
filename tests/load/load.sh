2040ef9d58c0ab9c1672a025bbb9d8898a1c96ff
Andrew Waters <waters@me.com>
2018-09-13 19:01:10 +0100

    Update report path

--- tests/load/load.sh
+++ tests/load/load.sh
@@ -5,4 +5,4 @@ echo "Starting load test... will take 1m"
 echo 'GET http://localhost:8081/bins' | \
     vegeta attack -rate 200 -duration 1m | vegeta encode > ./tests/load/results.json
 
-vegeta report results.*
+vegeta report ./tests/load/results.*
