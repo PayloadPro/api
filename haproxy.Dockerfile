8b4280b8151d9c475c889593b36a835674c73d20
Andrew Waters <waters@me.com>
2018-09-18 09:38:22 +0100

    Remove deployment info

--- haproxy.Dockerfile
+++
@@ -1,2 +0,0 @@
-FROM haproxy:1.7
-COPY deployments/haproxy-dev.cfg /usr/local/etc/haproxy/haproxy.cfg
--- tests/load/load.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Starting load test... will take 1m"
-
-echo 'GET http://localhost:8081/bins' | \
-    vegeta attack -rate 200 -duration 1m | vegeta encode > ./tests/load/results.json
-
-vegeta report ./tests/load/results.*
