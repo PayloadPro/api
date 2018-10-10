ec03f66fbe2ca13b8aa15bee7450b805f8df1413
Andrew Waters <waters@me.com>
2018-10-10 11:10:22 +0100

    Add a cockroach config test

---
+++ configs/cockroach_test.go
@@ -0,0 +1,20 @@
+package configs
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCockroachSetup(t *testing.T) {
+
+	DSN := "postgresql://user:pass@host:port/table"
+
+	os.Setenv("DB_DSN", DSN)
+
+	db := &CockroachConfig{}
+	db.Setup()
+
+	if db.DSN != DSN {
+		t.Errorf("Incorrect DSN. Got: `%s`, expected: `%s`", db.DSN, DSN)
+	}
+}
